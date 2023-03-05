598626f17d3bbfce6d0b434c2d2f8997129f9b6e
Jerebtw <jeremialp41@gmail.com>
2023-03-05 16:18:32 +0100

    Move to time and add format support

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,32 @@
-use chrono::{NaiveDate, Weekday};
+use time::{
+  format_description, Date,
+  Weekday::{Friday, Monday},
+};
 use wasm_bindgen::prelude::*;
 
 /// Returns true if the given year has a 53rd week.
 #[wasm_bindgen(js_name = "has53Weeks")]
-pub fn has_53_weeks(year: i32 /*** Test */) -> bool {
-  NaiveDate::from_isoywd_opt(year, 53, Weekday::Sun).is_some()
+pub fn has_53_weeks(year: i32) -> bool {
+  time::Date::from_iso_week_date(year, 53, Monday).is_ok()
 }
 
-/// Returns a string in the format "dd.mm.yyyy-dd.mm.yyyy" for the given year and week.
+/// Returns a string in the format "dd.mm.yyyy-dd.mm.yyyy" (Or a custom format if provided. https://time-rs.github.io/book/api/format-description.html) for the given year and week.
 /// The week is assumed to be a Monday-Friday week.
 /// Returns an error if the year/week is invalid.
 #[wasm_bindgen(js_name = "getDateStr")]
-pub fn get_date_str(year: i32, week: u32) -> Result<String, String> {
-  let mon =
-    NaiveDate::from_isoywd_opt(year, week, Weekday::Mon).ok_or_else(|| "Invalid year/week")?;
-  let fri =
-    NaiveDate::from_isoywd_opt(year, week, Weekday::Fri).ok_or_else(|| "Invalid year/week")?;
-  return Ok(format!(
-    "{}-{}",
-    mon.format("%d.%m.%Y"),
-    fri.format("%d.%m.%Y")
-  ));
+pub fn get_date_str(year: i32, week: u8, format: Option<String>) -> Result<String, String> {
+  let mon = Date::from_iso_week_date(year, week, Monday).map_err(|_| "Invalid year/week")?;
+  let fri = Date::from_iso_week_date(year, week, Friday).map_err(|_| "Invalid year/week")?;
+
+  let custom_format = format.unwrap_or_else(|| "[day].[month].[year]".to_string());
+  let format = format_description::parse(&custom_format).unwrap();
+  Ok(
+    format!(
+      "{}-{}",
+      mon.format(&format).unwrap(),
+      fri.format(&format).unwrap()
+    )
+    .trim()
+    .to_string(),
+  )
 }
--- tests/wasm.rs
+++ tests/wasm.rs
@@ -17,22 +17,47 @@ fn test_has_53_weeks() {
 fn test_get_date_str() {
   assert_eq!(
     "30.12.2019-03.01.2020".to_string(),
-    get_date_str(2020, 1).unwrap()
+    get_date_str(2020, 1, None).unwrap()
   );
   assert_eq!(
     "21.12.2020-25.12.2020".to_string(),
-    get_date_str(2020, 52).unwrap()
+    get_date_str(2020, 52, None).unwrap()
   );
   assert_eq!(
     "28.12.2020-01.01.2021".to_string(),
-    get_date_str(2020, 53).unwrap()
+    get_date_str(2020, 53, None).unwrap()
   );
   assert_eq!(
     "04.01.2021-08.01.2021".to_string(),
-    get_date_str(2021, 1).unwrap()
+    get_date_str(2021, 1, None).unwrap()
   );
   assert_eq!(
     "Invalid year/week".to_string(),
-    get_date_str(2021, 53).unwrap_err()
+    get_date_str(2021, 53, None).unwrap_err()
+  );
+}
+
+#[wasm_bindgen_test]
+fn test_get_date_str_custom_format() {
+  let custom_format = Some("[Year]-[Month]-[Day]".to_string());
+  assert_eq!(
+    "2019-12-30-2020-01-03".to_string(),
+    get_date_str(2020, 1, custom_format.clone()).unwrap()
+  );
+  assert_eq!(
+    "2020-12-21-2020-12-25".to_string(),
+    get_date_str(2020, 52, custom_format.clone()).unwrap()
+  );
+  assert_eq!(
+    "2020-12-28-2021-01-01".to_string(),
+    get_date_str(2020, 53, custom_format.clone()).unwrap()
+  );
+  assert_eq!(
+    "2021-01-04-2021-01-08".to_string(),
+    get_date_str(2021, 1, custom_format.clone()).unwrap()
+  );
+  assert_eq!(
+    "Invalid year/week".to_string(),
+    get_date_str(2021, 53, custom_format).unwrap_err()
   );
 }
